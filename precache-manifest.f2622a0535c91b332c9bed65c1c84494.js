3e122423eedb9a949c2a6154b608b2b5d8a08fb3
Berat Akgül <berat.akgul@alegradigital.com>
2020-03-14 20:07:46 +0300

    Updates

---
+++ precache-manifest.f2622a0535c91b332c9bed65c1c84494.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af59619b98f792f967ace6c6a281296b",
+    "url": "/lyricfinder/index.html"
+  },
+  {
+    "revision": "9b067b6f5af92f267ed4",
+    "url": "/lyricfinder/static/css/main.23353057.chunk.css"
+  },
+  {
+    "revision": "350451556807b41e02f4",
+    "url": "/lyricfinder/static/js/2.2de07492.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/lyricfinder/static/js/2.2de07492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b067b6f5af92f267ed4",
+    "url": "/lyricfinder/static/js/main.2e676228.chunk.js"
+  },
+  {
+    "revision": "4023d0e2e12112f50f14",
+    "url": "/lyricfinder/static/js/runtime-main.54e72eca.js"
+  }
+]);
